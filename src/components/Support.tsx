5f75fbab0ec8078424d5ed672cd0b6a3675c613f
Zach Killebrew <zbkilleb@gmail.com>
2022-12-01 16:12:37 -0600

    Add support modal

---
+++ src/components/Support.tsx
@@ -0,0 +1,61 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+} from '@mui/material';
+import { Coffee } from '@mui/icons-material';
+
+export function Support({
+  supportModalOpen,
+  handleSupportClick,
+}: {
+  supportModalOpen: boolean;
+  handleSupportClick: () => void;
+}) {
+  return (
+    <Dialog
+      open={supportModalOpen}
+      PaperProps={{
+        style: {
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: '#1b2127',
+          color: 'white',
+          border: 'solid white',
+          borderRadius: 10,
+        },
+      }}
+    >
+      <DialogTitle data-cy="SupportHeader">Support</DialogTitle>
+      <DialogContent data-cy="SupportContent">
+        <span>
+          Thank you so much for supporting this little game! If you have enjoyed
+          playing, please consider supporting to allow us to continue to improve
+          the game and add new features.
+        </span>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          data-cy="SupportClose"
+          variant="contained"
+          onClick={handleSupportClick}
+        >
+          Close
+        </Button>
+        <Button
+          data-cy="SupportButton"
+          variant="contained"
+          onClick={handleSupportLink}
+        >
+          Support <Coffee />
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+
+  function handleSupportLink() {
+    window.open('https://ko-fi.com/sixdob');
+  }
+}
