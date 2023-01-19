02cade9adb38a9e312ca47522ba8a270fe9dba82
Zach Killebrew <zbkilleb@gmail.com>
2023-01-19 17:08:56 -0600

    Move paperProps to constants file

---
+++ src/constants/styles.ts
@@ -0,0 +1,13 @@
+import { colors } from './colors';
+
+export const paperProps = {
+  style: {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center' as any,
+    backgroundColor: colors.paperBackground,
+    color: 'white',
+    border: 'solid white',
+    borderRadius: 10,
+  },
+};
